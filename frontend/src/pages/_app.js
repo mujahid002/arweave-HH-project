8e3d51db4d11435e5381c3c3dd7ff061bd65356d
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-14 18:43:15 +0530

    updated with Global Context component

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { GlobalContextProvider } from "../context/Store";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <GlobalContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GlobalContextProvider>
+  );
+}
