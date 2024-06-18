897da4bbec00c4ea9c65fb1de7bfaa08c20279b4
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-18 20:53:41 +0530

    warp SDK for contract deployment on AR

--- frontend/warp/configureWarpServer.js
+++ frontend/warp/configureWarpServer.js
@@ -1,38 +1,62 @@
 import { WarpFactory } from "warp-contracts";
-import { DeployPlugin } from "warp-contracts-plugin-deploy";
+import { DeployPlugin, ArweaveSigner } from "warp-contracts-plugin-deploy";
 import fs from "fs";
+// For passing to wallet param using the arweave.app wallet
+import { ArweaveWebWallet } from "arweave-wallet-connector";
 
-// environment can be 'local' | 'testnet' | 'mainnet' | 'custom';
+const webWallet = async () => {
+  const webWallet = new ArweaveWebWallet();
+  webWallet.setUrl("arweave.app");
+  await webWallet.connect();
+  return webWallet;
+};
 
-const environment = process.env.WARPENV || "testnet";
-let warp;
+const warp = WarpFactory.forTestnet().use(new DeployPlugin());
+//   const wallet = await configureWallet();
+const configureWallet = new ArweaveSigner(
+  JSON.parse(fs.readFileSync("./wallet.json", "utf-8"))
+);
+const transactionId = "Z7CnF20BePqBfHYPZGPe_JdnHjr2JjRCWCX29C9NJIQ";
 
-if (environment === "testnet") {
-  warp = WarpFactory.forTestnet().use(new DeployPlugin());
-} else if (environment === "mainnet") {
-  warp = WarpFactory.forMainnet().use(new DeployPlugin());
-} else {
-  throw Error("environment not set properly...");
-}
+const contractWithAdmin = warp.contract(transactionId).connect(configureWallet);
 
-async function configureWallet() {
-  try {
-    if (environment === "testnet") {
-      try {
-        return JSON.parse(fs.readFileSync("./testwallet.json", "utf-8"));
-      } catch (err) {
-        const { jwk } = await warp.generateWallet();
-        fs.writeFileSync("./testwallet.json", JSON.stringify(jwk));
-        return jwk;
-      }
-    } else if (environment === "mainnet") {
-      return JSON.parse(fs.readFileSync("../wallet.json", "utf-8"));
-    } else {
-      throw Error("Wallet not configured properly...");
-    }
-  } catch (err) {
-    throw Error("Wallet not configured properly...", err);
-  }
-}
+export { webWallet, warp, configureWallet, transactionId, contractWithAdmin };
 
-export { configureWallet, warp };
+// import { WarpFactory } from "warp-contracts";
+// import { DeployPlugin } from "warp-contracts-plugin-deploy";
+// import fs from "fs";
+
+// // environment can be 'local' | 'testnet' | 'mainnet' | 'custom';
+
+// const environment = process.env.WARPENV || "testnet";
+// let warp;
+
+// if (environment === "testnet") {
+//   warp = WarpFactory.forTestnet().use(new DeployPlugin());
+// } else if (environment === "mainnet") {
+//   warp = WarpFactory.forMainnet().use(new DeployPlugin());
+// } else {
+//   throw Error("environment not set properly...");
+// }
+
+// async function configureWallet() {
+//   try {
+//     if (environment === "testnet") {
+//       try {
+//         return JSON.parse(fs.readFileSync("./testwallet.json", "utf-8"));
+//       } catch (err) {
+//         const { jwk } = await warp.generateWallet();
+//         fs.writeFileSync("./testwallet.json", JSON.stringify(jwk));
+//         return jwk;
+//       }
+//     } else if (environment === "mainnet") {
+//       return JSON.parse(fs.readFileSync("../wallet.json", "utf-8"));
+//     } else {
+//       throw Error("Wallet not configured properly...");
+//     }
+//   } catch (err) {
+//     throw Error("Wallet not configured properly...", err);
+//   }
+// }
+
+// export { configureWallet, warp };
