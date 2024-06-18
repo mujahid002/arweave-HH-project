78feeb3c6cf81090590af5dbfac29d23e982a470
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-18 20:54:07 +0530

    deploy script for contract

--- frontend/warp/deploy.js
+++ frontend/warp/deploy.js
@@ -1,19 +1,19 @@
 import fs from "fs";
 import { WarpFactory } from "warp-contracts";
 import { DeployPlugin, ArweaveSigner } from "warp-contracts-plugin-deploy";
-// import { configureWallet, warp } from "./configureWarpServer.js";
+import { configureWallet, warp } from "./configureWarpServer.js";
 
 async function deploy() {
-  const warp = WarpFactory.forTestnet().use(new DeployPlugin());
-  //   const wallet = await configureWallet();
-  const wallet = new ArweaveSigner(
-    JSON.parse(fs.readFileSync("./wallet.json", "utf-8"))
-  );
+  // const warp = WarpFactory.forTestnet().use(new DeployPlugin());
+  // //   const wallet = await configureWallet();
+  // const wallet = new ArweaveSigner(
+  //   JSON.parse(fs.readFileSync("./wallet.json", "utf-8"))
+  // );
   const state = fs.readFileSync("token-POWE.json", "utf-8");
   const contractsource = fs.readFileSync("Polyweave.js", "utf-8");
 
   const { contractTxId, srcTxId } = await warp.deploy({
-    wallet: wallet,
+    wallet: configureWallet,
     initState: state,
     src: contractsource,
   });
