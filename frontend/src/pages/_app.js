1d972a67adbe63159df479281d90186c307c189a
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-18 20:52:29 +0530

    updated with ArweaveKit

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { GlobalContextProvider } from "../context/Store";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
 
 export default function App({ Component, pageProps }) {
   return (
-    <GlobalContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GlobalContextProvider>
+    <ArweaveWalletKit>
+      <GlobalContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GlobalContextProvider>
+    </ArweaveWalletKit>
   );
 }
