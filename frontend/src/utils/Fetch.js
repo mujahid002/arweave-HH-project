54facc34d7cda2f5128987d4add92c8190050ba8
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-18 20:53:19 +0530

    Fetch matic price (for future ref)

---
+++ frontend/src/utils/Fetch.js
@@ -0,0 +1,26 @@
+import { useGlobalContext } from "@/context/Store";
+
+export async function fetchMaticPrice() {
+  const { setMaticPrice } = useGlobalContext();
+  try {
+    // Fetch MATIC price
+    const maticDataResponse = await fetch(
+      `https://api.polygonscan.com/api?module=stats&action=maticprice&apikey=${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`
+    );
+    if (!maticDataResponse.ok) {
+      throw new Error("Failed to fetch MATIC price");
+    }
+    const maticData = await maticDataResponse.json();
+    const maticPrice = parseFloat(maticData.result.maticusd);
+    if (isNaN(maticPrice)) {
+      throw new Error("Invalid MATIC price data");
+    }
+
+    setMaticPrice(maticPrice.toFixed(4));
+
+    return;
+  } catch (error) {
+    console.error("Error fetching prices:", error);
+    return { error: error.message };
+  }
+}
